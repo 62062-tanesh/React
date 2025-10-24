5709ae07f47c848b1f990f374a47474977b2471c
62062-tanesh <tanesh8409@gmail.com>
2025-10-24 16:13:22 +0530

    Working on the project

--- BG-Removal Project/client/src/components/Upload.jsx
+++ BG-Removal Project/client/src/components/Upload.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Upload = () => {
   return (
-    <div>Upload</div>
+    <div className='pb-16'>
+        <h1 className='text-center text-2xl md:text-3xl lg:text-4xl mt-4 font-semibold bg-gradient-to-r from-gray-900 to-gray-400 bg-clip-text text-transparent py-6 md:py-16'>See the Magic. Try Now</h1>
+
+        
+
+    </div>
   )
 }
 
