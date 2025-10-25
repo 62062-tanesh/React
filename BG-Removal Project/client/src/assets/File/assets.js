28bec7fcc5972f4232ca0bc6f57d48e18e67e1d8
62062-tanesh <tanesh8409@gmail.com>
2025-10-25 16:16:51 +0530

    changing the name of the Testomonials

--- BG-Removal Project/client/src/assets/File/assets.js
+++ BG-Removal Project/client/src/assets/File/assets.js
@@ -40,14 +40,14 @@ export const testimonialsData = [
     {
         id: 1,
         text: "I've been using bg.removal for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.",
-        author: "Ranjeet Kumar",
+        author: "Ranjeet Raj",
         image: profile_img_1,
         jobTitle:'Web Developer'
     },
     {
         id: 2,
         text: "I've been using bg.removal for nearly 6 months, I had a fantastic experience. The quality is top-notch. I recommend others to try this app.",
-        author: "Rishav Raj",
+        author: "Rishav Kumar",
         image: profile_img_2,
         jobTitle:'UI Deginer'
     },
