05101be51ff6a7416acc7cc1a909afb55cd4d632
62062-tanesh <tanesh8409@gmail.com>
2025-11-07 22:45:00 +0530

    Add gitignore file

--- BG-Removal-Project/server/configs/mongodb.js
+++ BG-Removal-Project/server/configs/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async()=> {
     mongoose.connection.on('connected',()=>{
-        console.log("Database Connected");
+        console.log("Database Connected Fine");
     })
     await mongoose.connect(`${process.env.MONGODB_URI}/bg-removal`)
 }
