a9c7c01b9da40e274ef00a6f295b55b052e48f4b
62062-tanesh <tanesh8409@gmail.com>
2026-06-11 22:15:37 +0530

    learning Props

--- react-learning/src/App.jsx
+++ react-learning/src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Bookmark } from "lucide-react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <div className="card">
+        <div className="top">
+          <img src="https://logopng.com.br/logos/amazon-2.png" alt="" />
+          <button>
+            Save <Bookmark />
+          </button>
+        </div>
+        <div className="center">
+          <h3>Amzon</h3>
+          <span>Senior UI/UX Designer</span>{" "}
+          <div className="content1">Part-TIme</div>
+          <div className="content2">Senior Level</div>
+        </div>
+        <div className="bottom"></div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
