9aa3d79f2575e369adf5811ef9c7d25e9c58569f
62062-tanesh <tanesh8409@gmail.com>
2026-06-11 23:20:27 +0530

    Learning about Props

--- react-learning/src/App.jsx
+++ react-learning/src/App.jsx
@@ -8,16 +8,26 @@ const App = () => {
         <div className="top">
           <img src="https://logopng.com.br/logos/amazon-2.png" alt="" />
           <button>
-            Save <Bookmark />
+            Save <Bookmark size={12} color="white" />
           </button>
         </div>
         <div className="center">
-          <h3>Amzon</h3>
-          <span>Senior UI/UX Designer</span>{" "}
-          <div className="content1">Part-TIme</div>
-          <div className="content2">Senior Level</div>
+          <h3>
+            Amazon <span>5 days ago</span>{" "}
+          </h3>
+          <h2>Senior UI/UX Designer</h2>
+          <div className="tag">
+            <h4>Part time</h4>
+            <h4>Senior Level</h4>
+          </div>
         </div>
-        <div className="bottom"></div>
+        <div className="bottom">
+          <div>
+            <h3>$120/hr</h3>
+            <p>Mumbai, India</p>
+          </div>
+          <button>Apply Now</button>
+        </div>
       </div>
     </div>
   );
