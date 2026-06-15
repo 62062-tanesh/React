6f37b7e25270c1f1f4e8d55299a7757308a8cbad
62062-tanesh <tanesh8409@gmail.com>
2026-06-15 23:23:35 +0530

    Creating a Card

--- react-learning/src/App.jsx
+++ react-learning/src/App.jsx
@@ -5,24 +5,60 @@ const App = () => {
   return (
     <div className="parent">
       <div className="card">
-        <div className="top">
-          <img src="https://pngimg.com/uploads/amazon/amazon_PNG5.png" alt="" />
-          <button>
-            Save <Bookmark size={15}/>
-          </button>
-        </div>
-        <div className="center">
-          <h3>
-            Amazon <span>5 days Ago</span>
-          </h3>
-          <h2>Senior UI/UX Designer</h2>
-          <div>
-            <h4>part Time</h4>
-            <h4>Senior level</h4>
+        <div>
+          <div className="top">
+            <img
+              src="https://pngimg.com/uploads/amazon/amazon_PNG5.png"
+              alt=""
+            />
+            <button>
+              Save <Bookmark size={15} />
+            </button>
+          </div>
+          <div className="center">
+            <h3>
+              Amazon <span>5 days Ago</span>
+            </h3>
+            <h2>Senior UI/UX Designer</h2>
+            <div className="tag">
+              <h4>Part Time</h4>
+              <h4>Senior level</h4>
+            </div>
           </div>
         </div>
         <div className="bottom">
-          <div className="tag">
+          <div>
+            <h3>$120/hr</h3>
+            <p>Mumbai, India</p>
+          </div>
+          <button>Apply Now</button>
+        </div>
+      </div>
+
+      <div className="card">
+        <div>
+          <div className="top">
+            <img
+              src="https://pngimg.com/uploads/amazon/amazon_PNG5.png"
+              alt=""
+            />
+            <button>
+              Save <Bookmark size={15} />
+            </button>
+          </div>
+          <div className="center">
+            <h3>
+              Amazon <span>5 days Ago</span>
+            </h3>
+            <h2>Senior UI/UX Designer</h2>
+            <div className="tag">
+              <h4>Part Time</h4>
+              <h4>Senior level</h4>
+            </div>
+          </div>
+        </div>
+        <div className="bottom">
+          <div>
             <h3>$120/hr</h3>
             <p>Mumbai, India</p>
           </div>
