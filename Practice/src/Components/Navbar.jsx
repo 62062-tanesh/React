202d633a5bebec647f26ab6ebc5de723d3227795
62062-tanesh <tanesh8409@gmail.com>
2026-06-18 21:45:14 +0530

    New Codes

---
+++ Practice/src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex bg-green-300 px-2 py-4 items-center'>
+        <img src="https://geekybar.com/wp-content/uploads/2022/02/swetasinghat_251938178_2076524632500192_1910677256060375833_n-e1644648915637.jpg" alt="img" className=' h-10 w-10 rounded-full border-2 border-amber-50' />
+    <h3 className='ml-5 font-bold'>SWETA</h3>
+    <h2 className='px-4 py-1 bg-gray-200 rounded-full ml-320'>Login/Signup</h2>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ Practice/src/Components/Section1.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Section1 = () => {
+  return (
+    <div>
+        <div className='h-100 bg-amber-100 '>
+            <p className='text-3xl font-extralight uppercase px-20 mt-10'>This is Sweta</p>
+            <h3 className='text-5xl font-bold uppercase px-20 py-20'>as a Graphic Designer</h3>
+        </div>
+    </div>
+  )
+}
+
+export default Section1
