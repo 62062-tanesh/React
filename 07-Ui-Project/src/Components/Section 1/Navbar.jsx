5d97eb8343b023dc9af79b1b672ae7f715c14c7a
62062-tanesh <tanesh8409@gmail.com>
2026-06-21 23:26:13 +0530

    Adding a message

--- 07-Ui-Project/src/Components/Section 1/Navbar.jsx
+++ 07-Ui-Project/src/Components/Section 1/Navbar.jsx
@@ -4,6 +4,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between py-4 px-10 text-white text-1xl text' >
         <h2 className='bg-black text-white px-4 py-2 rounded-full uppercase'>Target Audience</h2>
+        <h2 className='text-green-400 uppercase font-bold'>Home</h2>
         <button className=' bg-gray-200 text-black px-4 py-2 rounded-full uppercase tracking-wider text-sm'>Digital Banking Platfrom</button>
 
 
