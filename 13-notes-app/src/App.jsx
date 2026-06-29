20902ac694e1fea88c3b4f9428b5d2758522032f
62062-tanesh <tanesh8409@gmail.com>
2026-06-29 22:24:06 +0530

    Update

--- 13-notes-app/src/App.jsx
+++ 13-notes-app/src/App.jsx
@@ -1,31 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
 
+const [title, setTitle] = useState('')
 const submitHandler = (e) =>{
   e.preventDefault()
-
-  console.log("Form Submitted");
-  
+  console.log({title,detail});
+  setTitle('')
+  setDetail('')
 }
+
+const [detail, setDetail] = useState('')
+
 
   return (
     <div className="h-screen bg-gray-100 lg:flex">
-      <form action="" className="flex m-5 p-10 flex-col gap-8" onSubmit={(e)=>{
+      
+      <form action="" className="flex m-5 p-10 flex-col gap-4" onSubmit={(e)=>{
         submitHandler(e)
       }}>
+        <h1 className="font-bold text-3xl uppercase">Add Notes</h1>
        
-          <input type="text" placeholder="Enter Notes Heading"  className="p-5 border-2 rounded-xl"/>
-        <textarea placeholder="Enter Details" name="" id="" className="p-5 border-2 rounded-xl"></textarea>
+          <input type="text" placeholder="Enter Notes Heading"  className="p-5 border-2 rounded-xl" value={title}
+          onChange={(e)=>{
+            setTitle(e.target.value);
+            
+          }}
+          />
+        <textarea placeholder="Enter Details" name="" id="" className="p-5 border-2 rounded-xl" value={detail} onChange={(e)=>{
+          setDetail(e.target.value)
+        }}></textarea>
+
 
         <button className="bg-transparent text-black p-3 border-2">Add Note</button>
        
       </form>
-      <div className=" m-15 p-5 bg-black rounded-xl">
-        <h1 className="text-white text-2xl font-bold uppercase">Your Notes</h1>
-        <div className="flex flex-wrap gap-5 mt-5">
-          <div className="h-52 w-40 rounded-2xl bg-white"></div>
-        <div className="h-52 w-40 rounded-2xl bg-white"></div>
+      <div className=" m-15 p-10 lg:border-l-2">
+        <h1 className="text-black text-2xl font-bold uppercase">Your Notes</h1>
+        <div className="flex flex-wrap gap-5 mt-5 overflow-auto">
+          <div className="h-52 w-40 rounded-2xl bg-black"></div>
         </div>
         
       </div>
