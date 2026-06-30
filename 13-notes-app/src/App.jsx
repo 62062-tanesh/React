b096aaba9f0997765a6af954c79ab85b18613186
62062-tanesh <tanesh8409@gmail.com>
2026-06-30 22:44:32 +0530

    Update

--- 13-notes-app/src/App.jsx
+++ 13-notes-app/src/App.jsx
@@ -3,14 +3,20 @@ import React, { useState } from "react";
 const App = () => {
 
 const [title, setTitle] = useState('')
+const [detail, setDetail] = useState('')
+const [task, setTask] = useState([])
+
+
 const submitHandler = (e) =>{
   e.preventDefault()
-  console.log({title,detail});
+
+const copyTask = [...task];
+copyTask.push({title,detail})
+setTask(copyTask)
+
   setTitle('')
   setDetail('')
 }
-
-const [detail, setDetail] = useState('')
 
 
 
@@ -33,13 +39,17 @@ const [detail, setDetail] = useState('')
         }}></textarea>
 
 
-        <button className="bg-transparent text-black p-3 border-2">Add Note</button>
+        <button className="bg-transparent text-black p-3 border-2 active:bg-gray-500">Add Note</button>
        
       </form>
       <div className=" m-15 p-10 lg:border-l-2">
-        <h1 className="text-black text-2xl font-bold uppercase">Your Notes</h1>
+        <h1 className="text-black text-2xl font-bold uppercase">Recent Notes</h1>
         <div className="flex flex-wrap gap-5 mt-5 overflow-auto">
-          <div className="h-52 w-40 rounded-2xl bg-black"></div>
+          {task.map(function(ele,idx){
+            return <div key={idx} className="h-52 w-40 rounded-2xl bg-cover bg-[url('https://cdn5.vectorstock.com/i/1000x1000/02/89/realistic-spiral-notebook-mockup-vector-27020289.jpg')] text-white p-5 text-xl">
+              <h2 className="text-2xl font-bold text-black p-2">{ele.title}</h2> <hr className="text-black w-full" /><p className="text-red-500">{ele.detail}</p>
+            </div>
+          })}
         </div>
         
       </div>
