cde47b38c2e80bfdc81b800c7d6cd8c0dbe1c5b1
62062-tanesh <tanesh8409@gmail.com>
2026-07-01 21:45:09 +0530

    Learning

--- 14-local storage/src/App.jsx
+++ 14-local storage/src/App.jsx
@@ -2,12 +2,21 @@ import React from 'react'
 
 const App = () => {
 
-console.log(localStorage.getItem("User"));
+const user = {
+  userName : "Tanesh",
+  Roll_No : "12",
+  Class : "Four"
+}
+
+localStorage.setItem("User",JSON.stringify(user))
+
+
+console.log(JSON.parse(localStorage.getItem("User")));
 
 
   return (
     <div>App</div>
-    
+
   )
 }
 
